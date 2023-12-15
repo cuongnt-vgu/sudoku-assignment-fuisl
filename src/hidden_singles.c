ef8aa1401ba7ccca183144df2fad2c7827a59587
James Tran <135719107+fuisl@users.noreply.github.com>
2023-12-15 15:28:30 +0000

    Co-authored-by: Cuong Tuan Nguyen <ntcuong2103@users.noreply.github.com>

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -1,6 +1,21 @@
 #include "hidden_singles.h"
+#include <stdio.h>
 
 int hidden_singles(SudokuBoard *p_board)
 {
+    // for (int i  = 0; i < BOARD_SIZE; i++)
+    // {
+    //     for (int j = 0; j < BOARD_SIZE; j++)
+    //     {
+    //         int * candidates = get_candidates(&(p_board->data[i][j]));
+    //         for (int k = 0; k < p_board->data[i][j].num_candidates; k++)
+    //         {
+    //             printf("%d", candidates[k]);
+    //         }
+    //         printf("\t");
+    //         free(candidates);
+    //     }
+    //     printf("\n");
+    // }
     return 0;
 }
